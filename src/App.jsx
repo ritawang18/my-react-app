8085d317d7b99386dbd22f1569ce52519a3bf99b
ritawang18 <97565824+ritawang18@users.noreply.github.com>
2025-04-03 12:37:10 -0500

    Here's the finished version of the react tic-tac-toe game!

--- src/App.jsx
+++ src/App.jsx
@@ -69,9 +69,9 @@ function calculateWinner(squares) {
 }
 
 export default function Game() {
-  const xIsNext=currentMove %2===0;
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext=currentMove %2===0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
